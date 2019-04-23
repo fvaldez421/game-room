9bdfb7b0deedc2f9b16c469e275008085bfddc3e
Frank Valdez <fvaldez421@gmail.com>
2019-04-22 23:15:33 -0700

    added base home/dashboard pagge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Dashboard = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+const Games = styled.div`
+  height: calc(100% - 2px);
+  display: inline-block;
+  width: calc(100% - 204px);
+  border: 1px solid white;
+`;
+const Scores = styled.div`
+  height: calc(100% - 2px);  
+  display: inline-block;
+  width: 200px;
+  border: 1px solid white;
+`;
 
 class HomePage extends Component {
   constructor(props) {
@@ -9,7 +27,14 @@ class HomePage extends Component {
   }
   render() {
     return (
-      <div>Hello There</div>
+      <Dashboard>
+        <Games>
+          Games Go Here
+        </Games>
+        <Scores>
+          Scores Go Here
+        </Scores>
+      </Dashboard>
     )
   }
 }
