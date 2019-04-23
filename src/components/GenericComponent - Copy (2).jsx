ea06e711432b91f0eae97738729bc9b9a54025bd
Frank Valdez <fvaldez421@gmail.com>
2019-04-22 23:15:01 -0700

    added base components

---
+++ src/components/GenericComponent - Copy (2).jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const OuterDiv = styled.div`
+  position: relative;
+`
+
+const GenericComponent = () => {
+  return (
+    <OuterDiv>
+      THIS IS SAMPLE TEXT
+    </OuterDiv>
+  )
+}
+
+export default GenericComponent;
---
+++ src/components/StyleHelpers.js
@@ -0,0 +1,9 @@
+/**
+ * style helpers
+ */
+
+export const position = ({ position = 'relative' }) => position
+export const margin = ({ margin = '0' }) => margin
+export const padding = ({ padding = '0' }) => padding;
+export const float = ({ float = 'none' }) => float
+
