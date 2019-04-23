e59265b588cd777fbf5a24ac7376e1c51814efd6
Frank Valdez <fvaldez421@gmail.com>
2019-04-22 23:15:53 -0700

    added local storage helper and api helper

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+/**
+ * api helper tool
+ */
+
+// import { get, put, post, remove } from 'axios';
+
+export default {
+  login(params) {
+    return {
+      id: '2084358638732458',
+      name: 'Frank Valdez',
+      email: 'fvaldez421@gmail.com',
+      location: 'San Francisco, CA',
+      memScores: ['4', '8', '6'],
+      snakeScores: ['35', '67', '84'],
+      hangmanScores: ['92', '138', '221'],
+    }
+  },
+}
