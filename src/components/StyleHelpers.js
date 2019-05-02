0d5282040fdb7ecbd6310456800fe1b781a15cee
Frank Valdez <fvaldez421@gmail.com>
2019-05-02 00:00:17 -0700

    refactored and made more reusable

--- src/components/StyleHelpers.js
+++ src/components/StyleHelpers.js
@@ -8,11 +8,11 @@ export const margin = ({ margin = '0' }) => margin
 export const padding = ({ padding = '0' }) => padding;
 export const float = ({ float = 'none' }) => float
 
-export const growX = fullWidth => keyframes`
+export const shiftX = (from, to) => keyframes`
   from {
-    width: 0px;
+    width: ${from + 'px'};
   }
   to {
-  width: ${fullWidth + 'px'};
+    width: ${to + 'px'};
   }
 `;
