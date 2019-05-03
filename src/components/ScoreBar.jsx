fdb5d24eac4fd12b3febadc224d90223f0fe5593
Frank Valdez <fvaldez421@gmail.com>
2019-05-02 19:58:57 -0700

    added styles to scores bar and improved style helper terminology

--- src/components/ScoreBar.jsx
+++ src/components/ScoreBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { shiftX } from './StyleHelpers';
+import { resizeWidth } from './StyleHelpers';
 
 
 const ScoresContainer = styled.div`
@@ -8,32 +8,34 @@ const ScoresContainer = styled.div`
   position: relative;
   display: inline-block;
   background: slategrey;
-  /* border: 1px solid #777; */
   border: ${({ expanded=false }) => expanded ? '1px solid #bbb' : 'none'};
+  border-top: none;
+  border-bottom: none;
   width: ${({ expanded = false }) => (expanded ? '270' : '0') + 'px'};
-  animation: ${({ expanded = false }) => shiftX(expanded ? '0' : '270', expanded ? '270' : '0')} .5s 1;
-  > h3 {
-    width: 100%;
-    margin: 10px 0;
-  }
+  animation: ${({ expanded = false }) => resizeWidth(expanded ? '0' : '270', expanded ? '270' : '0')} .5s 1;
 `;
 const ToggleClicker = styled.button`
   position: absolute;
+  display: flex;
   z-index: 10;
   top: 0;
   right: calc(100% + 1px);
-  width: auto;
   padding: 6px 8px 6px 12px;
   font-weight: 600;
   background: #fff;
   border: 1px solid #777;
   border-right: none;
   border-radius: 10px 0 0 10px;
+  width: ${({ expanded = false }) => (expanded ? '29' : '65') + 'px'};
+  animation: ${({ expanded = false }) => resizeWidth(expanded ? '65' : '29', expanded ? '29' : '65')} .5s 1;
   &:hover {
     cursor: pointer;
   }
   &:focus {
     outline: none;
+  }
+  > span {
+    overflow: hidden;
   }
 `;
 const ScoresWrap = styled.div`
@@ -42,14 +44,21 @@ const ScoresWrap = styled.div`
   width: calc(100% - 30px);
   overflow: hidden;
   white-space: nowrap;
+  > h3 {
+    width: 100%;
+    margin: 10px 0;
+    line-height: 14px;
+  }
 `;
 
 const ScoreBar = ({ user }) => {
   const [barExpanded, toggleBar] = useState(false);
   return (
     <ScoresContainer expanded={barExpanded} >
-      <ToggleClicker onClick={() => toggleBar(!barExpanded)}>
-        {barExpanded ? '>' : 'Scores'}
+      <ToggleClicker expanded={barExpanded} onClick={() => toggleBar(!barExpanded)}>
+        <span>
+          {barExpanded ? '>' : 'Scores'}
+        </span>
       </ToggleClicker>
       <ScoresWrap>
         <h3>Welcome {user.username}!</h3>
--- src/components/StyleHelpers.js
+++ src/components/StyleHelpers.js
@@ -8,7 +8,7 @@ export const margin = ({ margin = '0' }) => margin
 export const padding = ({ padding = '0' }) => padding;
 export const float = ({ float = 'none' }) => float
 
-export const shiftX = (from, to) => keyframes`
+export const resizeWidth = (from, to) => keyframes`
   from {
     width: ${from + 'px'};
   }
